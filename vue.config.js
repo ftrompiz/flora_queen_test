0194d99b04cdb8a38a6541f60a38dbd4a310fc5e
Francisco Trompiz <trober131@gmail.com>
2021-10-27 13:18:20 +0200

    - cambio de la configuracion del proxy, ahora apunta al servidor de heroku

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '^/api': {
-                target: 'http://localhost:500',
+                target: 'https://bouquets.herokuapp.com',
                 changeOrigin: true,
                 logLevel: 'debug',
                 pathRewrite: { '^/api': '/' },
